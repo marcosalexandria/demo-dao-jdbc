b515906fdc4e2c2fc2a80a179b98002faa039d99
Marcos Alexandria <marcos.alexandria01@gmail.com>
2022-06-10 17:15:51 -0300

    Department entity class

---
+++ src/aplication/Program.java
@@ -0,0 +1,15 @@
+package aplication;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department dp = new Department(1, "books");
+		
+		System.out.println(dp);
+
+	}
+
+}
