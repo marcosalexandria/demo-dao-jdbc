0848a89cbc7ca54dcc611bca183eb3f87cdf9393
Marcos Alexandria <marcos.alexandria01@gmail.com>
2022-06-11 16:36:13 -0300

    seller

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,14 +1,19 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-
+		
 		Department dp = new Department(1, "books");
 		
-		System.out.println(dp);
+		Seller seller = new Seller(21, "Bob", "bob@email.com", new Date(), 3000.0, dp);
+		
+		System.out.println(seller);
 
 	}
 
