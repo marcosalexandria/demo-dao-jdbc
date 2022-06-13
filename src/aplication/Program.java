ba40faab79c6f4d127157d36fd44187df07e606a
Marcos Alexandria <marcos.alexandria01@gmail.com>
2022-06-13 18:26:08 -0300

    seller

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -2,6 +2,8 @@ package aplication;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		Department dp = new Department(1, "books");
 		
 		Seller seller = new Seller(21, "Bob", "bob@email.com", new Date(), 3000.0, dp);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 
